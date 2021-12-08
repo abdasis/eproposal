5c746b9ad46f0e2bd68a6e00dbb7e3e501181c28
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2021-12-09 05:44:56 +0700

    tambah filter pada halaman dashboard

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -11,7 +11,7 @@ class Dashboard extends Component
     public function render()
     {
         return view('livewire.dashboard', [
-            'dataProposal' => Proposal::where('dibuat_oleh', \Auth::id())->take(5),
+            'dataProposal' => Proposal::where('dibuat_oleh', \Auth::id())->get()->take(5),
             'dataStakeHolder' => Anggota::all()->take(5)
         ]);
     }
